d5b660707d317e8dae0917d783564c3923999bba
jeffmun <61306390+Jumshim@users.noreply.github.com>
2024-05-09 17:24:41 -0700

    jm - completed help request table, the tests, the storybook, and confirmed 100% mutation coverage

---
+++ frontend/src/stories/components/HelpRequest/HelpRequest.stories.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { currentUserFixtures } from "fixtures/currentUserFixtures";
+import { rest } from "msw";
+import HelpRequestTable from "main/components/HelpRequest/HelpRequestTable";
+import { helpRequestFixtures } from "fixtures/helpRequestFixtures";
+
+export default {
+  title: "components/HelpRequest/HelpRequestTable",
+  component: HelpRequestTable,
+};
+
+const Template = (args) => {
+  return <HelpRequestTable {...args} />;
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+  helpRequests: [],
+};
+
+export const ThreeItemsOrdinaryUser = Template.bind({});
+
+ThreeItemsOrdinaryUser.args = {
+  helpRequests: helpRequestFixtures.threeHelpRequests,
+  currentUser: currentUserFixtures.userOnly,
+};
+
+export const ThreeItemsAdminUser = Template.bind({});
+ThreeItemsAdminUser.args = {
+  helpRequests: helpRequestFixtures.threeHelpRequests,
+  currentUser: currentUserFixtures.adminUser,
+};
+
+ThreeItemsAdminUser.parameters = {
+  msw: [
+    rest.delete("/api/helprequests", (req, res, ctx) => {
+      window.alert("DELETE: " + JSON.stringify(req.url));
+      return res(ctx.status(200), ctx.json({}));
+    }),
+  ],
+};
